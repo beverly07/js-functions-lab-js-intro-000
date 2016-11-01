f7ed762b273683f238cdd23dc8bec2bdbf14afdf
beverly <beverlybribiesca07@gmail.com>
2016-11-01 18:08:36 +0000

    newww message

---
+++ index.js
@@ -0,0 +1,22 @@
+//write you code below
+var happyHolidays = function(){
+  return 'Happy holidays!';
+};
+
+var happyHolidaysTo = function(){
+  return 'Happy holidays, you!';
+};
+
+var happyHolidaysTo = function(){
+  return 'Happy ${holiday}, ${name}!;
+};
+
+
+
+
+
+
+
+
+
+
